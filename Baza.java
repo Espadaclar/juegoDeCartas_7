b1593ba626c545c3ad7ce37072b104408a1769ad
Espadaclar <eneroborde8002@gmail.com>
2017-02-05 23:07:58 +0100

    addCarta(.., ..) añade cartas a la baza.

--- Baza.java
+++ Baza.java
@@ -10,8 +10,9 @@ import java.util.ArrayList;
 class Baza {
     private int numJugadores; // ---ndica el número de jugadores que van a tirar carta.
     private int paloQuePinta; //----es otro entero que indica el palo que pinta.
-
-
+    private String nombreJugador;
+    
+    private ArrayList<Carta> bazasDelJugador;
     /**
      * constructor para inicializar los atributos.
      * @param numJugadores
@@ -20,9 +21,20 @@ class Baza {
     public Baza(int numJugadores, int paloQuePinta) {
         this.numJugadores = numJugadores;
         this.paloQuePinta = paloQuePinta;
-
+        nombreJugador = "";
+        
+        bazasDelJugador = new ArrayList<>();//---- para poder añadir cartas a la baza.
     }
 
-
+    /**
+     * El primer parámetro representa la carta que tira un jugador a la mesa y que, por 
+     * tanto, se añade a la baza. No hace falta comprobar que la carta añadida 
+     * sea o no null, se supone que es una carta. El segundo parámetro indica el
+     * nombre del jugador que tiro la carta
+     */
+    public void addCarta(Carta carta, String nombreJ){
+        bazasDelJugador.add(carta);//----  añade cartas a la baza.
+        nombreJugador = nombreJ;
+    }
 }
 
